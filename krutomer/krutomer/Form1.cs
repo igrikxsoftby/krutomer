cfe5a462494b3e304db9ff0fdb77d126a5b81f67
IgrikX <igrikxsoft@tut.by>
2011-01-11 01:41:38 +0200

    тут я добавил кнопку, по нажатию которой выскакивает окно!

--- krutomer/krutomer/Form1.cs
+++ krutomer/krutomer/Form1.cs
@@ -15,5 +15,10 @@ namespace krutomer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Всмысле?");
+        }
     }
 }
